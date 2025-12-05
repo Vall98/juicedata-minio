59f4bacdb0fa63c7fd4169b9b08419e40affdee3
Valentin Lebon <valentin.lebon@epitech.eu>
2025-12-05 20:38:20 +0000

    [FIX] browser: add basename to router (removed with history)

--- browser/app/index.js
+++ browser/app/index.js
@@ -33,7 +33,7 @@ const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/minio">
       <App />
     </BrowserRouter>
   </Provider>
