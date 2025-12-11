8b55a14ddcf362158ff62c407770ce73f655810e
Valentin Lebon <valentin.lebon@epitech.eu>
2025-12-11 18:37:56 +0000

    [FIX] cmd/web-router: added fonts to special assets

--- cmd/web-router.go
+++ cmd/web-router.go
@@ -49,7 +49,7 @@ func (h indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 const assetPrefix = "production"
 
 // specialAssets are files which are unique files not embedded inside index_bundle.js.
-const specialAssets = "index_bundle.*.js|loader.css|logo.svg|firefox.png|safari.png|chrome.png|favicon-16x16.png|favicon-32x32.png|favicon-96x96.png"
+const specialAssets = "index_bundle.*.js|loader.css|logo.svg|firefox.png|safari.png|chrome.png|favicon-16x16.png|favicon-32x32.png|favicon-96x96.png|.*.woff2|.*.woff|.*.ttf|.*.eot|.*.svg"
 
 // registerWebRouter - registers web router for serving minio browser.
 func registerWebRouter(router *mux.Router) error {
